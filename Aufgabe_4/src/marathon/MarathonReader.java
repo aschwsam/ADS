38040bbf5cf8d7e015b1f8c3c1e66cfbf87917ef
aschwsam <aschwsam@students.zhaw.ch>
2017-03-16 15:32:04 +0100

    CSV Parser done
    CSV Parser implementiert

--- Aufgabe_4/src/marathon/MarathonReader.java
+++ Aufgabe_4/src/marathon/MarathonReader.java
@@ -1,9 +1,21 @@
-package marathon;
-
+import java.util.ArrayList;
 
 /**
  * Created by stephan on 16.03.17.
  */
 public class MarathonReader {
-
+	
+	private ArrayList<Competitor> athlets;
+	
+	public static void main (String Args[]){
+		MarathonReader asd = new MarathonReader();
+	}
+	
+	public MarathonReader(){
+		athlets = CSVParser.readFile();
+		
+		for(Competitor at : athlets){
+			System.out.println(at.getFirstName());
+		}
+	}
 }
