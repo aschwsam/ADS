2a0ac2c304b4c7850c060d37b0cd15cd80724e7f
stephan <stphngrf@gmail.com>
2017-03-29 17:19:32 +0200

    add id argument to addNode

--- p05/DirectedGraph.java
+++ p05/DirectedGraph.java
@@ -16,7 +16,7 @@ public interface DirectedGraph {
     public int getNumberOfEdges();
     public ArrayList<Node> getNodeList();
     public ArrayList<Edge> getEdgeListForNode(Node node);
-    public void addNode();
+    public void addNode(String id);
     public void removeNode(Node node);
     public void addEdge(Node from, Node to);
     public void addEdge(Node from, Node to, int weight);
