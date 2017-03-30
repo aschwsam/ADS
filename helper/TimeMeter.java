f593cea60c2188940ee4d0689ac04dddf2256fe6
stephan <stphngrf@gmail.com>
2017-03-30 14:06:51 +0200

    add Time Meter

---
+++ helper/TimeMeter.java
@@ -0,0 +1,22 @@
+package helper;
+
+
+import javax.annotation.PostConstruct;
+import java.lang.reflect.Method;
+
+
+/**
+ * Created by stephan on 30.03.17.
+ */
+public class TimeMeter {
+
+    public static int measure(Object object, String methodName){
+        Class c = object.getClass();
+        for (Method method : c.getDeclaredMethods()) {
+            if (method.getAnnotation(PostConstruct.class) != null) {
+                System.out.println(method.getName());
+            }
+        }
+        return 0;
+    }
+}
