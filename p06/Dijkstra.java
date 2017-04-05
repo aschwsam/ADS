d0ce04f2bc992ba90ba91ed4ec4313a6b52f4200
aschwsam <sam.aschwanden@gmail.com>
2017-04-05 14:14:58 +0200

    started p06

--- p06/Dijkstra.java
+++ p06/Dijkstra.java
@@ -1,13 +1,36 @@
 package p06;
 import p05.*;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 /**
- * Created by Sam on 05.04.2017.
+ * Our Implementation of the Dijkstra algorithm
  */
 public class Dijkstra {
 
+    private HashMap<Node,Integer> path = new HashMap<>();
+
     public static void main(String Args[]){
 
+        // initialize the mash
+        Graph graph = new Graph();
     }
 
+    private void getNonVisitedNodes(Node origin){
+
+        // Temporary store for neighbours
+        HashMap<Node,Integer> candidate = new HashMap<>();
+
+        for(Edge edge : origin.getEdges()){
+            if(!path.containsKey(edge.getDestination()) || !path.containsValue(edge.getDestination())){
+
+                // Temporary store all neighbours
+                candidate.put(edge.getDestination(),edge.getWeight());
+            }
+        }
+
+        // TODO: Select the neighbour with the shortest distance
+
+    }
 }
