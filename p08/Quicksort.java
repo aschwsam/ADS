87d5baee76bc91ca0e20bf7c2045bda5d7378a43
aschwsam <sam.aschwanden@gmail.com>
2017-04-25 02:05:25 +0200

    Quicksort "one level"

--- p08/Quicksort.java
+++ p08/Quicksort.java
@@ -8,46 +8,49 @@ package p08;
 public class Quicksort {
 
     public static void main(String Args[]){
-        int[] numbers = {1,2,3,4,5,6,7,8,9};
-        //sort(numbers);
+        int[] numbers = {4,3,2,7,2,8,5,6,1};
+        sort(numbers);
     }
 
     public static void sort(int[] numbers){
 
         if(numbers.length==1 || numbers.length<1){
             return;
+        } else {
+            sort(numbers,0,numbers.length-1);
+            for(int asd : numbers){
+                System.out.println(asd);
+            }
         }
+    }
 
+    public static void sort(int[] numbers,int low_index, int high_index){
         // Define pivot
-         int pivot_position = (int)Math.floor(numbers.length/2);
-         int pivot_value = numbers[pivot_position];
+        int pivot_value = numbers[(int)Math.floor(numbers.length/2)];
 
-        int low_index = 0;
-        int high_index = numbers.length-1;
+        boolean move_element = true;
 
-         boolean move_element = true;
+        while(move_element){
 
-         while(move_element){
+            // Find bigger number OR pivot
+            while(low_index<high_index && numbers[low_index]<pivot_value){
+                low_index++;
+            }
 
-             // Find bigger number OR pivot
-             while(low_index<high_index && numbers[low_index]<pivot_value){
-                 low_index++;
-             }
+            while(high_index>low_index && numbers[high_index]>pivot_value){
+                high_index--;
+            }
 
-             while(high_index>low_index && numbers[high_index]>pivot_value){
-                 high_index--;
-             }
+            // DEBUG
+            System.out.println("Swap low: "+numbers[low_index]+" ("+low_index+") with high: "+numbers[high_index]+" ("+high_index+")");
 
-             // DEBUG
-             System.out.println("Swap low: "+low_index+" ("+numbers[low_index]+") with high: "+high_index+" ("+numbers[high_index]+")");
+            int tmp = numbers[low_index];
+            numbers[low_index] = numbers[high_index];
+            numbers[high_index] = tmp;
 
-             int tmp = numbers[low_index];
-             numbers[low_index] = numbers[high_index];
-             numbers[high_index] = tmp;
-
-             if(numbers[low_index] == numbers[high_index]){
-                 move_element=false;
-             }
-         }
+            if(numbers[low_index] == numbers[high_index]){
+                move_element=false;
+            }
+        }
     }
 }
